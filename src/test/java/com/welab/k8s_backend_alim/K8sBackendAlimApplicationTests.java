f1c11b745844d728fecffc22688e5a6e1901059b
suin <73993670+whl5105@users.noreply.github.com>
2025-06-13 00:38:21 +0900

    test 코드 삭제

--- src/test/java/com/welab/k8s_backend_alim/K8sBackendAlimApplicationTests.java
+++ src/test/java/com/welab/k8s_backend_alim/K8sBackendAlimApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class K8sBackendAlimApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
